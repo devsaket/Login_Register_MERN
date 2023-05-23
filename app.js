b7873ec6b49ab3b64b8dbfe3fbbddf1b526fd653
devsaket <dmsaketk@gmail.com>
2023-05-23 14:15:43 +0530

    node mongo connection done

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+const cors = require('cors');
+
+const connectDB = require('./server/database/connection')
+
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+
+
+dotenv.config({ path: 'config.env' })
+const PORT = process.env.PORT || 5000
+
+// log requests
+app.use(morgan('tiny'))
+
+// parse request to body-parser
+app.use(bodyparser.urlencoded({ extended: true }))
+
+//mongoose link
+connectDB();
+
+// Routes 
+
+
+
+app.listen(PORT, () =>{
+    console.log(`Server is Running at http://localhost:${PORT}`)
+})
